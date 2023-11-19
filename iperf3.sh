2840c614b7be390b2164badc734cbd1dbe3f36fc
Leonardo Chuello <ljchuello@gmail.com>
2023-11-19 17:34:07 -0500

    Update iperf3.sh

--- iperf3.sh
+++ iperf3.sh
@@ -13,3 +13,15 @@ apt install iperf3 -y
 # .NET Core LTS
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel LTS --install-dir /usr/share/dotnet
 ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
+
+# Glances
+apt install python3
+apt install python3-dev -y
+apt install python3-pip -y
+wget -O- https://bit.ly/glances | /bin/bash
+  
+# Glances Service
+cd /etc/systemd/system/
+wget -O glances-iqx5u.service "https://raw.githubusercontent.com/DeployRise/OnServer/main/glances-iqx5u.service"
+systemctl start glances-iqx5u
+systemctl enable glances-iqx5u
