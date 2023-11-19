39d3f23c2d49d6e31353e25ecf8201b52b9d6ece
Leonardo Chuello <ljchuello@gmail.com>
2023-11-19 16:17:10 -0500

    Create OnServer.min.sh

---
+++ OnServer.min.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Updates the packages
+DEBIAN_FRONTEND=noninteractive apt-get update && DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
+
+# Tools
+apt install curl -y
+apt install wget -y
+apt install unzip -y
+apt install nginx -y
+
+# .NET Core LTS
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel LTS --install-dir /usr/share/dotnet
+ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
