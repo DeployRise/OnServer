65d1f8e2f5ee22af5a1f10b8be2487b95f80b59b
Leonardo Chuello <ljchuello@gmail.com>
2023-11-21 20:56:54 -0500

    Update OnServer.sh

--- OnServer.sh
+++ OnServer.sh
@@ -3,15 +3,15 @@
 # Updates the packages
 DEBIAN_FRONTEND=noninteractive apt-get update && DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 
+# .NET Core LTS
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel LTS --install-dir /usr/share/dotnet
+ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
+
 # Tools
 apt install curl -y
 apt install wget -y
 apt install unzip -y
 apt install nginx -y
-
-# .NET Core LTS
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel LTS --install-dir /usr/share/dotnet
-ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
 
 # Firewall
 apt install ufw -y
