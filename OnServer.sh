3d269a6523e1789bf6e051b794c00ece56f7cf1d
Leonardo Chuello <ljchuello@gmail.com>
2023-12-04 22:52:28 -0500

    Update OnServer.sh

--- OnServer.sh
+++ OnServer.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Skip UFW
-SKIP_UFW=false
-while getopts ":skipufw" opt; do
-  case $opt in
-    skipufw) SKIP_UFW=true ;;
-    \?) ;;
-  esac
-done
-
 # Updates the packages
 DEBIAN_FRONTEND=noninteractive apt-get update && DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 
@@ -23,25 +14,22 @@ apt install iperf3 -y
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel LTS --install-dir /usr/share/dotnet
 ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
 
-# Firewall
-if [ "$SKIP_UFW" = false ]; then
-  # Install Firewall
-  apt install ufw -y
-  echo "y" | ufw enable
-    
-  # Enabling traffic from the internet
-  ufw allow 22/tcp
-  ufw allow 80/tcp
-  ufw allow 443/tcp
-  ufw allow 27017/tcp
-fi
+# Install Firewall
+apt install ufw -y
+echo "y" | ufw enable
+
+# Enabling traffic from the internet
+ufw allow 22/tcp
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw allow 27017/tcp
 
 # Glances
 apt install python3
 apt install python3-dev -y
 apt install python3-pip -y
 wget -O- https://raw.githubusercontent.com/nicolargo/glancesautoinstall/master/install.sh | /bin/bash
-  
+
 # Glances Service
 cd /etc/systemd/system/
 wget -O glances.service https://raw.githubusercontent.com/DeployRise/OnServer/main/glances.service
