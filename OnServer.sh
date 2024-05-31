1d9f6ab5321605230c9e1cc6e960b62a18b88b93
Leonardo Chuello <ljchuello@gmail.com>
2024-05-30 21:52:00 -0500

    Update OnServer.sh

--- OnServer.sh
+++ OnServer.sh
@@ -12,6 +12,20 @@ apt install nano -y
 apt install openssl -y
 wget -O -  https://get.acme.sh | sh -s email=lalo@landa.com
 
+# Firewall
+apt install ufw -y
+echo "y" | ufw enable
+ufw allow 22/tcp
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw allow 27017/tcp
+
+# Index & Certificates
+rm /var/www/html/index.nginx-debian.html
+wget -O /var/www/html/index.html https://raw.githubusercontent.com/DeployRise/OnServer/main/tools/index.html
+wget -O /etc/ssl/certificate.crt https://raw.githubusercontent.com/DeployRise/OnServer/main/tools/certificate.crt
+wget -O /etc/ssl/certificate.key https://raw.githubusercontent.com/DeployRise/OnServer/main/tools/certificate.key
+
 # .NET Core LTS
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel LTS --install-dir /usr/share/dotnet
 ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
@@ -27,32 +41,13 @@ apt install python3-dev -y
 apt install python3-pip -y
 apt install gcc python3-dev -y
 
-# Install Firewall
-apt install ufw -y
-echo "y" | ufw enable
-
-# Enabling traffic from the internet
-ufw allow 22/tcp
-ufw allow 80/tcp
-ufw allow 443/tcp
-ufw allow 27017/tcp
-
-# Index & Certificates
-rm /var/www/html/index.nginx-debian.html
-wget -O /var/www/html/index.html https://raw.githubusercontent.com/DeployRise/OnServer/main/index.html
-wget -O /etc/ssl/certificate.crt https://raw.githubusercontent.com/DeployRise/OnServer/main/certificate/certificate.crt
-wget -O /etc/ssl/certificate.key https://raw.githubusercontent.com/DeployRise/OnServer/main/certificate/certificate.key
-
 # Chrony
 apt install chrony -y
 systemctl enable chrony
 systemctl start chrony
 
-# Glances
-pip install glances[all] --break-system-packages
-
 # Glances Service
-cd /etc/systemd/system/
-wget -O glances.service https://raw.githubusercontent.com/DeployRise/OnServer/main/glances.service
+pip install glances[all] --break-system-packages
+wget -O /etc/systemd/system/glances.service https://raw.githubusercontent.com/DeployRise/OnServer/main/glances.service
 systemctl start glances
 systemctl enable glances
