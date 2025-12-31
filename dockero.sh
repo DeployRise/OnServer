51783dfb660c4c70b560ad64f1832a8a1a71f74c
Leonardo Chuello <ljchuello@gmail.com>
2025-12-30 22:56:02 -0400

    Refactor dockero.sh to simplify setup and use Docker
    Removed commented firewall and Python installation commands. Updated Glances service setup to use Docker.

--- dockero.sh
+++ dockero.sh
@@ -9,22 +9,8 @@ apt install wget -y
 apt install unzip -y
 apt install nano -y
 
-# Firewall
-# apt install ufw -y
-# echo "y" | ufw enable
-# ufw allow 22/tcp
-# ufw allow 80/tcp
-# ufw allow 81/tcp
-# ufw allow 443/tcp
-
 # Docker
 wget -O- https://raw.githubusercontent.com/DeployRise/OnServer/main/docker-install.sh | sudo /bin/bash
-
-# Python 3
-apt install python3 -y
-apt install python3-dev -y
-apt install python3-pip -y
-apt install gcc python3-dev -y
 
 # Chrony
 apt install chrony -y
@@ -32,12 +18,4 @@ systemctl enable chrony
 systemctl start chrony
 
 # Glances Service
-# pip install glances[all] --break-system-packages
-pip install glances[all] --break-system-packages --ignore-installed --force-reinstall urllib3
-wget -O /etc/systemd/system/glances.service https://raw.githubusercontent.com/DeployRise/OnServer/main/glances.service
-systemctl start glances
-systemctl enable glances
-
-# IPv4 --force
-sed -i 's/#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  100/' /etc/gai.conf
-systemctl restart networking
+docker run -d --name glances --restart always --pid host --network host -e GLANCES_OPT="-w -p 27017 -t 3" nicolargo/glances:latest-full
