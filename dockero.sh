b7470c37e275a863da8263391fcc35ca82ea1fb6
Leonardo Chuello <ljchuello@gmail.com>
2026-01-16 18:01:40 -0400

    Enhance dockero.sh with firewall rules for Docker
    Added firewall configuration for Docker and Nginx Proxy Manager.

--- dockero.sh
+++ dockero.sh
@@ -9,20 +9,38 @@ apt install wget -y
 apt install unzip -y
 apt install nano -y
 
-# Firewall
-apt install ufw -y
-echo "y" | ufw enable
-ufw allow 22/tcp
-ufw allow 80/tcp
-ufw allow 443/tcp
-
 # Docker
 wget -O- https://raw.githubusercontent.com/DeployRise/OnServer/main/docker-install.sh | sudo /bin/bash
+docker network create ljchuello
 
 # Chrony
 apt install chrony -y
 systemctl enable chrony
 systemctl start chrony
 
-# Glances Service
+# Firewall
+apt install ufw -y
+echo "y" | ufw enable
+ufw allow 22/tcp
+
+# Firewall Docker
+wget -O /usr/local/bin/ufw-docker https://github.com/chaifeng/ufw-docker/raw/master/ufw-docker
+chmod +x /usr/local/bin/ufw-docker
+ufw-docker allow nginxproxymanager 80/tcp
+ufw-docker allow nginxproxymanager 443/tcp
+ufw-docker allow nginxproxymanager 81/tcp
+
+# Glances
 docker run -d --name glances --restart always --pid host --network host -e GLANCES_OPT="-w -p 27017 -t 3" nicolargo/glances:latest-full
+
+ # Nginx Proxy Manager
+ docker run -d \
+  --name nginxproxymanager \
+  --restart unless-stopped \
+  --network ljchuello \
+  -p 80:80 \
+  -p 81:81 \
+  -p 443:443 \
+  -v "/root/nginxproxymanager/data:/data" \
+  -v "/root/nginxproxymanager/letsencrypt:/etc/letsencrypt" \
+  docker.io/jc21/nginx-proxy-manager:latest
